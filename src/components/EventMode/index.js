6300625c1faf948bcb38fbbb78ce1c95b49f12ca
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-20 11:55:29 -0300

    feat: Generic component for the sub title

--- src/components/EventMode/index.js
+++ src/components/EventMode/index.js
@@ -1,9 +1,11 @@
+import SubTitleTypography from './SubTitleTypography';
 import TitleTypography from './TitleTypography';
 
 export default function EventMode() {
   return (
     <>
       <TitleTypography title={'Ingressos e pagamento'} />
+      <SubTitleTypography title={'Primeiro, escolha sua modalidade de ingresso'} />
     </>
   );
 }
