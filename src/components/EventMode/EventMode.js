480d6318bdf9b54596e9898b5d0f43b4309a6449
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-20 22:17:00 -0300

    style: Selects online event and displays the order closing

--- src/components/EventMode/EventMode.js
+++ src/components/EventMode/EventMode.js
@@ -1,21 +1,33 @@
 import styled from 'styled-components';
 import Event from './Event';
 import useToggle from '../../hooks/useToggle';
+import EventOnline from './EventOnline';
 
 export default function EventMode() {
   const { firstOption, secondOption, selectFirstOption, selectSecondOption } = useToggle();
+  let price = '800';
 
   return (
     <EventContainer>
-      <Event type={'Presencial'} price={'R$ 350'} isSelected={firstOption} callback={selectFirstOption}/>
-      <Event type={'Online'} price={'R$ 100'} isSelected={secondOption} callback={selectSecondOption}/>
+      <ModalityEvent>
+        <Event type={'Presencial'} price={`R$ ${price}`} isSelected={firstOption} callback={selectFirstOption} />
+        <Event type={'Online'} price={`R$ ${price}`} isSelected={secondOption} callback={selectSecondOption} />
+      </ModalityEvent>
+      {secondOption ? <EventOnline price={price} /> : <></>}
     </EventContainer>
   );
 }
 
-const EventContainer = styled.div`
+const EventContainer = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ModalityEvent = styled.div`
   display: flex;
   flex: 1;
   flex-direction: row;
   gap: 0 24px;
+  margin-bottom: 44px;
 `;
