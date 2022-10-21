bb5d2a96082cbc4502714023b665a8f18f604319
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-20 21:00:58 -0300

    feat: Selects event mode 'presential' or 'online'

--- src/components/EventMode/index.js
+++ src/components/EventMode/index.js
@@ -1,11 +1,16 @@
 import SubTitleTypography from '../SubTitleTypography';
 import TitleTypography from '../TitleTypography';
+import EventMode from './EventMode';
+import { EventWrapper } from './EventWrapper';
 
 export default function IngressPayment() {
   return (
     <>
       <TitleTypography title={'Ingressos e pagamento'} />
       <SubTitleTypography title={'Primeiro, escolha sua modalidade de ingresso'} />
+      <EventWrapper>
+        <EventMode />
+      </EventWrapper>
     </>
   );
 }
