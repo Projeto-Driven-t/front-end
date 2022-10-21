caecf69bec5687c996d7408283b491baebcf6313
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-21 11:58:00 -0300

    feat: Updates the state with the selected event data

--- src/components/EventMode/EventOnline.js
+++ src/components/EventMode/EventOnline.js
@@ -1,13 +1,21 @@
 import styled from 'styled-components';
+import useEventModeContext from '../../contexts/EventModeContext';
 import Button from '../Form/Button';
 import SubTitleTypography from '../SubTitleTypography';
 
-export default function EventOnline({ price }) {
+export default function EventOnline({ type, price }) {
+  const { setEventData } = useEventModeContext();
+
+  function closerOnlineOrder() {
+    setEventData({ type, price });
+  }
   return (
     <OrderContainer>
       <SubTitleTypography title={`Fechado! O total ficou em R$ ${price}. Agora é só confirmar:`} />
       <CloseOrderContainer>
-        <Button type="submit">Reservar Ingresso</Button>
+        <Button type="submit" onClick={() => closerOnlineOrder()}>
+          Reservar Ingresso
+        </Button>
       </CloseOrderContainer>
     </OrderContainer>
   );
