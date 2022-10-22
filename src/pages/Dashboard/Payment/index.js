0fb542ab850c02fba66d7d1f7f14af70bd68d7d6
Jorge Tadeu <jsbatalha@gmail.com>
2022-10-22 16:04:12 -0300

    feat: check user

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,7 +1,14 @@
 import TicketAndPayment from '../../../components/TicketAndPayment';
+import useEnrollment from '../../../hooks/api/useEnrollment';
+import TitleTypography from '../../../components/TitleTypography';
 
 export default function Payment() {
+  const { enrollment } = useEnrollment();
+  
   return (
-    <TicketAndPayment />
+    <>
+      <TitleTypography title={'Ingressos e pagamento'} />
+      {enrollment? <TicketAndPayment /> : <p>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</p>}
+    </>
   );
 }
