7276c307a8a87395533f999fa805b65c4b4aeec3
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-28 15:01:22 -0300

    feat: Creates the Hotel index

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,34 +1,29 @@
 import TitleTypography from '../../../components/TitleTypography';
 import useCheckTicket from '../../../hooks/api/useCheckTicket';
-import HotelAndRoom from '../../../components/Hotel';
 import React from 'react';
-import styled from 'styled-components';
 import { useEffect } from 'react';
 
-export default function Payment() {
+export default function Hotel() {
   const { checkTicket } = useCheckTicket();
   const [message, setMessage] = React.useState('');
 
-  React.useEffect(() => {
-    if(!checkTicket) {
+  useEffect(() => {
+    if (!checkTicket) {
       setMessage('Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem');
-    }else if(checkTicket.modality === 'Online' || checkTicket.accommodation === 'Sem Hotel') {
+    } else if (checkTicket.modality === 'Online' || checkTicket.accommodation === 'Sem Hotel') {
       setMessage('Sua modalidade de ingresso não inclui hospedagem. Prossiga para a escolha de atividades');
     }
   }, [checkTicket]);
 
   return (
-    <Container>
+    <>
       <TitleTypography title={'Escolha de Hotel e Quarto'} />
-      {
-        (checkTicket && checkTicket.modality === 'Presencial') || (checkTicket && checkTicket.accommodation === 'Com Hotel') ?
-          <HotelAndRoom />
-          :
-          message
-      }
-    </Container>
+      {(checkTicket && checkTicket.modality === 'Presencial') ||
+      (checkTicket && checkTicket.accommodation === 'Com Hotel') ? (
+        <HotelAndRoom />
+      ) : (
+        message
+      )}
+    </>
   );
 }
-
-const Container = styled.div`
-`;
