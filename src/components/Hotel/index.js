538969be816032c0b9312bd23be8442daadfefc0
MariaEmiliaCastro <emyly.mary@gmail.com>
2022-10-29 13:20:00 -0300

    feat: show hotel list

---
+++ src/components/Hotel/index.js
@@ -0,0 +1,10 @@
+import useHotelContext from '../../contexts/HotelContext';
+import Hotel from './Hotel';
+
+export default function HotelAndRoom() {
+  return (
+    <>
+      <Hotel />
+    </>
+  );
+}
