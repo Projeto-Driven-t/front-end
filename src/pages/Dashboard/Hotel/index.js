8a0ace2339091ac93b60d59960667a0b097fedf8
MariaEmiliaCastro <emyly.mary@gmail.com>
2022-10-29 11:12:34 -0300

    feat: add ticket type verification

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,34 @@
-export default function Hotel() {
-  return 'Hotel: Em breve!';
+import TitleTypography from '../../../components/TitleTypography';
+import useCheckTicket from '../../../hooks/api/useCheckTicket';
+import HotelAndRoom from '../../../components/Hotel';
+import React from 'react';
+import styled from 'styled-components';
+import { useEffect } from 'react';
+
+export default function Payment() {
+  const { checkTicket } = useCheckTicket();
+  const [message, setMessage] = React.useState('');
+
+  React.useEffect(() => {
+    if(!checkTicket) {
+      setMessage('Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem');
+    }else if(checkTicket.modality === 'Online' || checkTicket.accommodation === 'Sem Hotel') {
+      setMessage('Sua modalidade de ingresso não inclui hospedagem. Prossiga para a escolha de atividades');
+    }
+  }, [checkTicket]);
+
+  return (
+    <Container>
+      <TitleTypography title={'Escolha de Hotel e Quarto'} />
+      {
+        (checkTicket && checkTicket.modality === 'Presencial') || (checkTicket && checkTicket.accommodation === 'Com Hotel') ?
+          <p>Escolha de Hotel Coming Soon...</p>
+          :
+          message
+      }
+    </Container>
+  );
 }
+
+const Container = styled.div`
+`;
