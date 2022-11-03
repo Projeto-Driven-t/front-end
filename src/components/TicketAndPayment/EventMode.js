4e56ea3f7a27289658227c9d7f62e5a300249bfc
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-11-03 20:12:26 -0300

    fix: Fixes the toast bug when loading data

--- src/components/TicketAndPayment/EventMode.js
+++ src/components/TicketAndPayment/EventMode.js
@@ -27,11 +27,7 @@ export default function EventMode() {
   }
 
   useEffect(() => {
-    if (modality) {
-      renderModalities();
-    }
-
-    if(accommodation) {
+    if (modality && accommodation) {
       renderModalities();
     }
   }, [modality, accommodation]);
@@ -51,7 +47,11 @@ export default function EventMode() {
           );
         })}
       </ModalityContainer>
-      {selected.type === 'Presencial' ? <EventPresential type={selected.type} price={selected.price} accommodation={listOfAccommodations}/> : <></>}
+      {selected.type === 'Presencial' ? (
+        <EventPresential type={selected.type} price={selected.price} accommodation={listOfAccommodations} />
+      ) : (
+        <></>
+      )}
       {selected.type === 'Online' ? <EventOnline type={selected.type} price={selected.price} /> : <></>}
     </TicketContainer>
   );
