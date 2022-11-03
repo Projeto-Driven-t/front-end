02c557ead0ac23e38b5c047f2e00362eddfce2b0
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-11-03 16:36:34 -0300

    feat:  Displaying warnnigs

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,30 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import ChoiceOfActivities from '../../../components/Activities';
+import TitleTypography from '../../../components/TitleTypography';
+import useCheckTicket from '../../../hooks/api/useCheckTicket';
+import { Warning } from '../../../components/Auth';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const { checkTicket } = useCheckTicket();
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    if (!checkTicket) {
+      setMessage('Você precisa ter confirmado pagamento antes de fazer a escolha de atividades');
+    } else if (checkTicket.modality === 'Online') {
+      setMessage('Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades');
+    }
+  }, [checkTicket]);
+
+  return (
+    <>
+      <TitleTypography title={'Escolha de atividades'} />
+      {checkTicket && checkTicket.modality === 'Presencial' ? (
+        <ChoiceOfActivities />
+      ) : (
+        <Warning>{message}</Warning>
+      )}
+    </>
+  );
 }
